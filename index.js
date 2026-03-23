4361bd5c44eb8d7592ed36e03a95cf4e3106d4f4
noor-fullstack-developer <994455noor@gmail.com>
2026-03-23 16:09:53 +0530

    idex.js updated

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./config/db");
+const productRoutes = require("./routes/product.routes");
+
+const app = express();
+
+// Connect Database
+connectDB();
+
+// Middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cors());
+
+// Routes
+app.use("/api/products", productRoutes);
+
+// Server Start
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
