0d4cdbfa6234e2eb1dd87bebf3c71c6e16f2c280
noor-fullstack-developer <994455noor@gmail.com>
2026-03-23 16:14:09 +0530

    done

--- index.js
+++ index.js
@@ -17,8 +17,8 @@ app.use(cors());
 app.use("/api/products", productRoutes);
 
 // Server Start
-const PORT = process.env.PORT || 10000;
+const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, "0.0.0.0", () => {
+app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
